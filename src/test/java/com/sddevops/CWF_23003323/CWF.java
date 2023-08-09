7247b4e11cde7e0e51ec63fac8a775294bb6eeb3
agentfoxsg <narima.nasir@gmail.com>
2023-08-09 11:58:08 -0700

    commit next change

---
+++ src/test/java/com/sddevops/CWF_23003323/CWF.java
@@ -0,0 +1,19 @@
+package com.sddevops.CWF_23003323;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class CWF {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
