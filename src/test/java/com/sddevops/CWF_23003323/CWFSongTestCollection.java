622350a23739bc3fe7662f3dfb77dbb4ddc3fcca
agentfoxsg <narima.nasir@gmail.com>
2023-08-10 13:04:05 -0700

    Added Test Unit

---
+++ src/test/java/com/sddevops/CWF_23003323/CWFSongTestCollection.java
@@ -0,0 +1,73 @@
+package com.sddevops.CWF_23003323;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CWFSongTestCollection {
+	private SongCollection sc;
+	private Song s1;
+	private Song s2;
+	private Song s3;
+	private Song s4;
+	private final int SONG_COLLECTION_SIZE=4;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		sc=new SongCollection();
+		s1 = new Song("001","good 4 u","Olivia Rodrigo",3.59);
+		s2 = new Song("002","Peaches","Justin Bieber",3.18);
+		s3 = new Song("003","MONTERO","Lil Nas",2.3);
+		s4 = new Song("004","bad guy","billie eilish",3.14);
+		sc.addSong(s1);
+		sc.addSong(s2);
+		sc.addSong(s3);
+		sc.addSong(s4);
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetSongs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddSong() {
+		//fail("Not yet implemented");
+		List<Song> testSc=sc.getSongs();
+		//Assert that Song Collection is equals to Song Collection Size : 4
+		assertEquals(testSc.size(),SONG_COLLECTION_SIZE);
+		//Act
+		sc.addSong(s1);
+		//Assert that Song Collection is equals to Song Collection Size + 1 : 5
+		assertEquals(testSc.size(),SONG_COLLECTION_SIZE+1);
+	}
+
+	@Test
+	void testSortSongsByTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSortSongsBySongLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindSongsById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindSongByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
