c711a25e31d017081d8265d395b5cd0e976c2b37
agentfoxsg <narima.nasir@gmail.com>
2023-08-10 12:12:06 -0700

    Changes

---
+++ src/main/java/com/sddevops/CWF_23003323/App.java
@@ -0,0 +1,14 @@
+package com.sddevops.CWF_23003323;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("This is an informational message");
+    	System.err.println("This is an error message");;
+    }
+}
