20a19f3e22a163886ce2e0140b1159763bc9411d
agentfoxsg <narima.nasir@gmail.com>
2023-08-11 14:45:09 -0700

    added Junit Test

--- src/test/java/com/sddevops/CWF_23003323/CWFSongCollectionTest.java
+++ src/test/java/com/sddevops/CWF_23003323/CWFSongCollectionTest.java
@@ -15,82 +15,115 @@ public class CWFSongCollectionTest {
 	private Song s2;
 	private Song s3;
 	private Song s4;
-	private final int SONG_COLLECTION_SIZE=4; 
+	private final int SONG_COLLECTION_SIZE = 4;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		sc=new SongCollection();
-		s1 = new Song("001","good 4 u","Olivia Rodrigo",3.59);
-		s2 = new Song("002","Peaches","Justin Bieber",3.18);
-		s3 = new Song("003","MONTERO","Lil Nas",2.3);
-		s4 = new Song("004","bad guy","billie eilish",3.14);
+		sc = new SongCollection();
+		s1 = new Song("001", "good 4 u", "Olivia Rodrigo", 3.59);
+		s2 = new Song("002", "Peaches", "Justin Bieber", 3.18);
+		s3 = new Song("003", "MONTERO", "Lil Nas", 2.3);
+		s4 = new Song("004", "bad guy", "billie eilish", 3.14);
 		sc.addSong(s1);
 		sc.addSong(s2);
 		sc.addSong(s3);
 		sc.addSong(s4);
 	}
-	
+
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
 	@Test
 	void testGetSongs() {
-		//fail("Not yet implemented");
-		   List<Song> songs = sc.getSongs();
-	        assertEquals(SONG_COLLECTION_SIZE, songs.size());
-	        assertTrue(songs.contains(s1));
-	        assertTrue(songs.contains(s2));
-	        assertTrue(songs.contains(s3));
-	        assertTrue(songs.contains(s4));
+		// fail("Not yet implemented");
+		List<Song> songs = sc.getSongs();
+		assertEquals(SONG_COLLECTION_SIZE, songs.size());
+		assertTrue(songs.contains(s1));
+		assertTrue(songs.contains(s2));
+		assertTrue(songs.contains(s3));
+		assertTrue(songs.contains(s4));
 	}
-	
+
 	@Test
 	public void testAddSong() {
-		//fail("Not yet implemented");
-		List<Song> testSc=sc.getSongs();
-		//Assert that Song Collection is equals to Song Collection Size : 4
-		assertEquals(testSc.size(),SONG_COLLECTION_SIZE);
-		//Act
+		// fail("Not yet implemented");
+		List<Song> testSc = sc.getSongs();
+		// Assert that Song Collection is equals to Song Collection Size : 4
+		assertEquals(testSc.size(), SONG_COLLECTION_SIZE);
+		// Act
 		sc.addSong(s1);
-		//Assert that Song Collection is equals to Song Collection Size + 1 : 5
-		assertEquals(testSc.size(),SONG_COLLECTION_SIZE+1);
+		// Assert that Song Collection is equals to Song Collection Size + 1 : 5
+		assertEquals(testSc.size(), SONG_COLLECTION_SIZE + 1);
 	}
 
 	@Test
 	public void testSortSongsByTitle() {
-		//fail("Not yet implemented");
-		 ArrayList<Song> sortedSongs = sc.sortSongsByTitle();
-	        for (int i = 1; i < sortedSongs.size(); i++) {
-	            String previousTitle = sortedSongs.get(i - 1).getTitle();
-	            String currentTitle = sortedSongs.get(i).getTitle();
-	            assertTrue(previousTitle.compareTo(currentTitle) <= 0, "Songs are not sorted correctly by title");
-	        }
+		// fail("Not yet implemented");
+		ArrayList<Song> sortedSongs = sc.sortSongsByTitle();
+		for (int i = 1; i < sortedSongs.size(); i++) {
+			String previousTitle = sortedSongs.get(i - 1).getTitle();
+			String currentTitle = sortedSongs.get(i).getTitle();
+			assertTrue(previousTitle.compareTo(currentTitle) <= 0, "Songs are not sorted correctly by title");
+		}
 	}
 
-//	@Test
-//	void testSortSongsBySongLength() {
-//		//fail("Not yet implemented");
-//		  ArrayList<Song> sortedSongs = sc.sortSongsBySongLength();
-//	        for (int i = 1; i < sortedSongs.size(); i++) {
-//	            double previousLength = sortedSongs.get(i - 1).getSongLength();
-//	            double currentLength = sortedSongs.get(i).getSongLength();
-//	            assertTrue(previousLength <= currentLength, "Songs are not sorted correctly by song length");}
-//	        }
+	    @Test
+	    void testSortedSongsBySongLength_FirstTitle() {
+	        ArrayList<Song> sortedSongs = sc.sortSongsBySongLength();
+	        assertEquals(sortedSongs.get(0).getTitle(), "good 4 u");
+	    }
+
+	    @Test
+	    void testSortedSongsBySongLength_LastTitle() {
+	        ArrayList<Song> sortedSongs = sc.sortSongsBySongLength();
+	        assertEquals(sortedSongs.get(sortedSongs.size() - 1).getTitle(), "MONTERO");
+	    }
 
 	@Test
 	public void testFindSongsById() {
-		//fail("Not yet implemented");
-	      Song foundSong = sc.findSongsById("003");
-	        assertNotNull(foundSong);
-	        assertEquals("MONTERO", foundSong.getTitle());
-	    }
+		// fail("Not yet implemented");
+		Song foundSong = sc.findSongsById("003");
+		assertNotNull(foundSong);
+		assertEquals("MONTERO", foundSong.getTitle());
+	}
 
 	@Test
 	public void testFindSongByTitle() {
-		//fail("Not yet implemented");
-	      Song foundSong = sc.findSongByTitle("Peaches");
-	        assertNotNull(foundSong);
-	        assertEquals("Justin Bieber", foundSong.getArtiste());
-	    }
+		// fail("Not yet implemented");
+		Song foundSong = sc.findSongByTitle("Peaches");
+		assertNotNull(foundSong);
+		assertEquals("Justin Bieber", foundSong.getArtiste());
 	}
+
+	@Test
+	  void testGettersAndSetters() {
+        Song song = new Song("006", "Somewhere I Belong", "Linkin Park", 3.46);
+        
+        assertEquals("006", song.getId());
+        assertEquals("Somewhere I Belong", song.getTitle());
+        assertEquals("Linkin Park", song.getArtiste());
+        assertEquals(3.46, song.getSongLength());
+        
+        song.setId("007");
+        song.setTitle("Shivers");
+        song.setArtiste("Ed Sheeran");
+        song.setSongLength(3.58);
+        
+        assertEquals("007", song.getId());
+        assertEquals("Shivers", song.getTitle());
+        assertEquals("Ed Sheeran", song.getArtiste());
+        assertEquals(3.58, song.getSongLength());
+    }
+	@Test
+	void testHashCodeAndEquals() {
+	    Song song1 = new Song("006", "Somewhere I Belong", "Linkin Park", 3.46);
+	    Song song2 = new Song("006", "Somewhere I Belong", "Linkin Park", 3.46);
+	    Song song3 = new Song("007", "Easy On Me", "Adele", 5.32);
+	    assertEquals(song1.hashCode(), song2.hashCode());
+	    assertNotEquals(song1.hashCode(), song3.hashCode());
+	    assertEquals(song1, song2);
+	    assertNotEquals(song1, song3);
+	}
+}
+
